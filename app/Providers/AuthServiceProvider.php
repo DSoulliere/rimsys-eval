6a18b7caac6822dc22637b3fa5a48394d76b48ce
Derek Soulliere <derek.soulliere@gmail.com>
2022-11-22 03:40:47 -0500

    Add policies and admin guard bypass

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Models\Document;
+
+use App\Policies\Api\UserPolicy;
+use App\Policies\Api\DocumentPolicy;
+
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +20,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
+        Document::class => DocumentPolicy::class,
     ];
 
     /**
@@ -25,6 +33,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+
+        /**
+         * Register a gate to allow admins access to all resources
+         */
+        Gate::before(function ($user) {
+            if ($user->isAdmin()) {
+                return true;
+            }
+        });
     }
 }
