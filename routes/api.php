b6ee3d46b3dd18488c59d582da9347f3dc574dbb
Derek Soulliere <derek.soulliere@gmail.com>
2022-11-22 03:44:50 -0500

    Add all routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
+namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('users', [UserController::class, 'store']);
+Route::post('auth', [AuthController::class, 'store']);
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::delete('auth', [AuthController::class, 'destroy']);
+    Route::apiResource('users', UserController::class)->except('store');
+    Route::apiResource('documents', DocumentController::class);
+    Route::apiResource('users.documents', DocumentUserController::class)->only(['index', 'update', 'destroy']);
 });
