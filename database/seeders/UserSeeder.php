6c264826b352162de7035d902d5a1ecf249db7f9
Derek Soulliere <derek.soulliere@gmail.com>
2022-12-01 21:54:58 -0500

    Add default user account to seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,11 +15,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        //Creates a default admin
         User::firstOrCreate([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
             'password' => 'password',
             'is_admin' => true
+        ]);
+
+        //Creates a default user
+        User::firstOrCreate([
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'password' => 'password',
+            'is_admin' => false
         ]);
 
         User::factory()->count(10)->create();
