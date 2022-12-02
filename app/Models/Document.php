faefaebd24006395a4ddd68d03d1fb3ab8898ec9
Derek Soulliere <derek.soulliere@gmail.com>
2022-12-01 21:56:07 -0500

    Add file management methods

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -2,8 +2,14 @@
 
 namespace App\Models;
 
-
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Eloquent\MassAssignmentException;
+use Illuminate\Database\Eloquent\InvalidCastException;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
 
 class Document extends Model
 {
@@ -15,7 +21,11 @@ class Document extends Model
      */
     protected $fillable = [
         'name',
-        'content',
+        'description',
+        'disk',
+        'filepath',
+        'mimetype',
+        'filesize'
     ];
 
     /**
@@ -28,4 +38,58 @@ class Document extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+
+    /**
+     * Static method to create a model from the received file
+     *
+     * @param mixed $file
+     * @param array $attributes
+     * @param string|null $dest
+     * @param string $disk
+     * @return mixed
+     * @throws BindingResolutionException
+     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     */
+    static public function createFromFile($file, array $attributes, string $dest = 'files', string $disk = 'default')
+    {
+        $disk = config("filesystems.$disk");
+        $dest = $dest;
+
+        $filesize = filesize($file);
+        $mimetype = $file->getMimeType();
+        $filepath = Storage::disk($disk)->put($dest, $file);
+
+        return self::create(array_merge($attributes, compact('disk', 'filesize', 'filepath', 'mimetype')));
+    }
+
+
+    /**
+     * Takes a file and updates the model to new parameters
+     *
+     * @param mixed $file
+     * @param array $attributes
+     * @param string|null $dest
+     * @return bool
+     * @throws MassAssignmentException
+     * @throws InvalidArgumentException
+     * @throws InvalidCastException
+     */
+    public function updateFromFile($file, array $attributes, string $dest = null)
+    {
+        $dest = $dest;
+
+        $storage = Storage::disk($this->disk);
+        $storage->delete($this->filepath);
+
+        $filesize = filesize($file);
+        $mimetype = $file->getMimeType();
+        $filepath = $storage->put($dest, $file);
+
+        $this->fill(array_merge($attributes, compact('filesize', 'filepath', 'mimetype')));
+
+        return $this->save();
+    }
+
 }
